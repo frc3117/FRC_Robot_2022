e1b1df451286d8123298202ec85a833703f3cbfa
mickjosh <felix_roux@hotmail.com>
2022-02-04 18:42:51 -0500

    Fix IMU Constructor

--- src/main/java/frc/robot/Wrapper/ADIS16448_IMU_Gyro.java
+++ src/main/java/frc/robot/Wrapper/ADIS16448_IMU_Gyro.java
@@ -1,13 +1,14 @@
 package frc.robot.Wrapper;
 
 import edu.wpi.first.wpilibj.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class ADIS16448_IMU_Gyro implements Gyro
 {
     public ADIS16448_IMU_Gyro()
     {
-        _imu = new ADIS16448_IMU();
+        _imu = new ADIS16448_IMU(ADIS16448_IMU.IMUAxis.kZ, Port.kMXP, ADIS16448_IMU.CalibrationTime._1s);
     }
 
     private ADIS16448_IMU _imu;
